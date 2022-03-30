457999cf848c26ed3ea0386e0a6dc9f7d7361c0b
nikola rafajlovic <nikolaraf@hotmail.com>
2022-03-30 12:29:17 +0200

    Character Sorter Test Add

---
+++ tests/Unit/CharacterSorterTest.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Src\CharacterSorter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \OliverKlee\CodeKata\CharacterSorter
+ */
+final class CharacterSorterTest extends TestCase
+{
+    private CharacterSorter $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = new CharacterSorter();
+    }
+
+    /**
+     * @test
+     */
+    public function sortForEmptyStringReturnsEmptyString(): void
+    {
+        self::assertSame(
+            '',
+            $this->subject->sort('')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortForOneCharacterReturnsThatCharacter(): void
+    {
+        self::assertSame(
+            'h',
+            $this->subject->sort('h')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortForPresortedCharactersReturnsSortedCharacters(): void
+    {
+        self::assertSame(
+            'ab',
+            $this->subject->sort('ab')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortForTwoUnsortedCharactersReturnsTwoSortedCharacters(): void
+    {
+        self::assertSame(
+            'ab',
+            $this->subject->sort('ba')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortWithWhitespaceInsideDiscardsTheWhitespace(): void
+    {
+        self::assertSame(
+            'ab',
+            $this->subject->sort('b a')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortForTwiceTheSameCharacterShouldReturnTheCharacterTwice(): void
+    {
+        self::assertSame(
+            'aa',
+            $this->subject->sort('aa')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sortForCharsWithApostropheRemovesApostrophe(): void
+    {
+        self::assertSame(
+            'aa',
+            $this->subject->sort('a\'a')
+        );
+    }
+}
